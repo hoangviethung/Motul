1e55d66595d88a68298e57bf3c74f30b0985cc9c
hoangviethung <hoangviet.hung.009@gmail.com>
2019-08-06 14:32:45 +0700

    HÙNG UPDATE

--- src/js/gmap.js
+++ src/js/gmap.js
@@ -1,11 +1,10 @@
 function initialize() {
-
+	// Nếu mà vị trí bằng rỗng thì trả về Hồ Chí Minh
 	var latLocation, lngLocation;
-	debugger;
-	if(inputLocations.length != 0){
+	if (inputLocations.length != 0) {
 		latLocation = inputLocations[0].lat;
 		lngLocation = inputLocations[0].lng;
-	}else{
+	} else {
 		latLocation = '10.823181';
 		lngLocation = '106.629675';
 	}
@@ -16,6 +15,7 @@ function initialize() {
 		mapTypeId: google.maps.MapTypeId.ROADMAP,
 		styles: [{ "featureType": "all", "elementType": "labels.text.fill", "stylers": [{ "saturation": 36 }, { "color": "#000000" }, { "lightness": 40 }] }, { "featureType": "all", "elementType": "labels.text.stroke", "stylers": [{ "visibility": "on" }, { "color": "#000000" }, { "lightness": 16 }] }, { "featureType": "all", "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] }, { "featureType": "administrative", "elementType": "geometry.fill", "stylers": [{ "color": "#000000" }, { "lightness": 20 }] }, { "featureType": "administrative", "elementType": "geometry.stroke", "stylers": [{ "color": "#000000" }, { "lightness": 17 }, { "weight": 1.2 }] }, { "featureType": "landscape", "elementType": "geometry", "stylers": [{ "color": "#000000" }, { "lightness": 20 }] }, { "featureType": "landscape", "elementType": "labels.text", "stylers": [{ "saturation": "-6" }, { "lightness": "12" }] }, { "featureType": "poi", "elementType": "geometry", "stylers": [{ "color": "#000000" }, { "lightness": 21 }] }, { "featureType": "road", "elementType": "labels.text.fill", "stylers": [{ "saturation": "1" }, { "lightness": "-1" }, { "visibility": "on" }, { "color": "#a2a2a2" }] }, { "featureType": "road.highway", "elementType": "geometry", "stylers": [{ "saturation": "9" }, { "gamma": "0.93" }, { "visibility": "simplified" }] }, { "featureType": "road.highway", "elementType": "geometry.fill", "stylers": [{ "lightness": 17 }, { "visibility": "simplified" }, { "color": "#ED1D24" }] }, { "featureType": "road.highway", "elementType": "geometry.stroke", "stylers": [{ "color": "#000000" }, { "lightness": 29 }, { "weight": 0.2 }] }, { "featureType": "road.arterial", "elementType": "geometry", "stylers": [{ "color": "#000000" }, { "lightness": 18 }] }, { "featureType": "road.local", "elementType": "geometry", "stylers": [{ "color": "#000000" }, { "lightness": 16 }] }, { "featureType": "transit", "elementType": "geometry", "stylers": [{ "color": "#000000" }, { "lightness": 19 }] }, { "featureType": "water", "elementType": "geometry", "stylers": [{ "color": "#dedede" }, { "lightness": 17 }] }, { "featureType": "water", "elementType": "labels.text", "stylers": [{ "color": "#ffffff" }] }],
 	}
+
 	map = new google.maps.Map(document.getElementById("map"), mapOptions);
 	var locations = inputLocations;
 
@@ -28,6 +28,7 @@ function initialize() {
 	});
 
 
+	var bounds = new google.maps.LatLngBounds();
 	for (i = 0; i < locations.length; i++) {
 		marker = new google.maps.Marker({
 			position: new google.maps.LatLng(locations[i].lat, locations[i].lng),
@@ -51,6 +52,14 @@ function initialize() {
 		})(marker, i));
 
 		markers.push(marker);
+		var item1 = new google.maps.LatLng(locations[i].lat, locations[i].lng)
+
+		bounds.extend(item1);
+	}
+
+	// bounds.extend(inputLocation.lng);
+	if (locations.length > 1) {
+		map.fitBounds(bounds);
 	}
 }
 
